35cec2b9791f2191f987edcbdf3a3d5cd8fdf398
Marat Gumerov <gumerovm74@gail.com>
2019-05-31 14:53:53 +0300

    1) Unary operator 2) Ternary operator

--- src/java/by/epam/learn/main/LearnMainUnaryOperator.java
+++ src/java/by/epam/learn/main/LearnMainUnaryOperator.java
@@ -1,4 +1,32 @@
 package java.by.epam.learn.main;
 
 public class LearnMainUnaryOperator {
+    public static void main(String[] args) {
+
+        int x = 1;
+        x++; // 2
+        x = x + 1; //3
+
+        /*
+        префиксная форма  сначало присваивание потом сложение
+        int x =1;
+        ++x; //2
+
+        int y = ++x; // x = 2; y = 2;
+        int y = ++x; // x = 2; y = 1;
+
+        byte b = 1;
+        b = b + 1  // ошибка компиляции
+        b++;       //  приведение типов автоматически
+
+        int x = 1;
+        x = ++x;        //x = 2;
+
+        //если наоборот
+
+        x = x++           //x = 1;
+
+        */
+
+    }
 }
---
+++ src/java/by/epam/learn/main/LearnMaintTernary.java
@@ -0,0 +1,36 @@
+package java.by.epam.learn.main;
+
+
+/* необходимо назначить процент скидки покупателю от купленых товаров
+ * если количество товара больше 3 то скидка 10%
+ * если меньше либо равно 3 то скидки не будет.
+ */
+public class LearnMaintTernary {
+    public static void main(String[] args) {
+
+
+        int bonus;
+
+        int discount = 10;
+
+        int purchaseItem = 5;
+
+        final int DISCOUNT_LIMIT = 3;
+
+        // выражение с IF
+        //    if (purchaseItem > DISCOUNT_LIMIT) {
+        //        bonus = discount;
+        //    } else {
+        //        bonus = 0;
+        //      System.out.println(bonus);
+        //    }
+
+
+        //  тернальный оператор
+        bonus = purchaseItem > DISCOUNT_LIMIT ? discount: 0;
+
+    }
+}
+
+
+
